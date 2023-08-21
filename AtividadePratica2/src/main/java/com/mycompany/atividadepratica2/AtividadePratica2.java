3da0a05ada92886b1230a13a681a34d94dd8557e
LaraViggiano <lara.r.viggiano@gmail.com>
2023-08-21 19:55:46 -0300

    primeirocommit

---
+++ AtividadePratica2/src/main/java/com/mycompany/atividadepratica2/AtividadePratica2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividadepratica2;
+
+/**
+ *
+ * @author Larar
+ */
+public class AtividadePratica2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
