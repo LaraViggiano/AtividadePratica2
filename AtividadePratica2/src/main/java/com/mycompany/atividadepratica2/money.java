7352179025310f030c3454020cc3c71a4d521f54
LaraViggiano <lara.r.viggiano@gmail.com>
2023-08-21 20:37:02 -0300

    QuintaParte

--- AtividadePratica2/src/main/java/com/mycompany/atividadepratica2/money.java
+++ AtividadePratica2/src/main/java/com/mycompany/atividadepratica2/money.java
@@ -31,51 +31,29 @@ public class money {
    static Money franc(int amount) {
       return new Franc(amount);
     }
-   abstract String currency();
    String currency() {
       return currency;
    }
-   static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
-   }
-   
-   static Money franc(int amount) {
-      return new Franc(amount, "CHF");
-   }
-   abstract class Money {
-   private String currency; 
-
-   static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
+ static Money dollar(int amount)  {
+      return new Money(amount, "USD");
    }
 
    static Money franc(int amount) {
-      return new Franc(amount, "CHF");
+      return new Money(amount, "CHF");
    }
-
+    
    Money(int amount, String currency) {
       this.amount = amount;
       this.currency = currency;
    }
-}
-
-class Franc extends Money {	
-   Franc(int amount, String currency) {
-      super(amount, currency);
-   }
-     
-   Money times(int multiplier)  {
-      return Money.franc(amount * multiplier);
-   }
-}
-
-class Dollar extends Money {	
-   Dollar(int amount, String currency)  {
-      super(amount, currency);
+	
+   public boolean equals(Object object) {
+      Money money = (Money) object;
+      return amount == money.amount && currency().equals(money.currency());
    }
 	
-   Money times(int multiplier)  {
-      return Money.dollar(amount * multiplier);
+   Money times(int multiplier) {
+      return new Money(amount * multiplier, currency);
    }
    
 }
