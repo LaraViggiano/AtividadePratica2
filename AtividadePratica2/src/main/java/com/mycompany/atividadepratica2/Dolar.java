b110a3f9323de7e2da1d31b270b0632296c1d73c
LaraViggiano <lara.r.viggiano@gmail.com>
2023-08-21 20:01:46 -0300

    primeiraparte

---
+++ AtividadePratica2/src/main/java/com/mycompany/atividadepratica2/Dolar.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividadepratica2;
+
+/**
+ *
+ * @author Larar
+ */
+public class Dolar {
+    
+}
